2b163981f6736195bdac6bb1f8b5f4a0f1e434ba
rudolfssau-classroom <125464967+rudolfssau-classroom@users.noreply.github.com>
2023-03-19 23:26:27 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,32 +1,20 @@
 # python3
-import concurrent
 
 def parallel_processing(n, m, data):
-    output = []
+    m
     threads = [0] * n
-    with concurrent.futures.ThreadPoolExecutor(max_workers=n) as executor:
-        futures = []
-        for value in data:
-            future = executor.submit(process_value, value, threads)
-            futures.append(future)
-        for future in concurrent.futures.as_completed(futures):
-            result = future.result()
-            output.append(result)
-    return output
-
-def process_value(value, threads):
-    thread_index = threads.index(min(threads))
-    threads[thread_index] += value
-    return (thread_index, threads[thread_index])
-
+    result = []
+    for d in data:
+        worker_index = threads.index(min(threads))
+        result.append([worker_index, threads[worker_index]])
+        threads[worker_index] += d
+    return result
 def main():
-    imp = list(map(int, input().split()))
-    n = imp[0]
-    m = imp[1]
+    n, m = map(int, input().split())
     data = list(map(int, input().split()))
     result = parallel_processing(n, m, data)
-    for value in result:
-        print(value[0], value[1])
+    for i, t in result:
+        print(i, t)
 
 if __name__ == "__main__":
     main()
