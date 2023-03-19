0d8b52ab0fc5c99564c664883c58fdf3c2b3b592
rudolfssau-classroom <125464967+rudolfssau-classroom@users.noreply.github.com>
2023-03-19 23:14:03 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,31 +1,32 @@
 # python3
+import concurrent
 
 def parallel_processing(n, m, data):
     output = []
-    # TODO: write the function for simulating parallel tasks, 
-    # create the output pairs
-
+    threads = [0] * n
+    with concurrent.futures.ThreadPoolExecutor(max_workers=n) as executor:
+        futures = []
+        for value in data:
+            future = executor.submit(process_value, value, threads)
+            futures.append(future)
+        for future in concurrent.futures.as_completed(futures):
+            result = future.result()
+            output.append(result)
     return output
 
+def process_value(value, threads):
+    thread_index = threads.index(min(threads))
+    threads[thread_index] += value
+    return (thread_index, threads[thread_index])
+
 def main():
-    # TODO: create input from keyboard
-    # input consists of two lines
-    # first line - n and m
-    # n - thread count 
-    # m - job count
-    n = 0
-    m = 0
-
-    # second line - data 
-    # data - contains m integers t(i) - the times in seconds it takes any thread to process i-th job
-    data = []
-
-    # TODO: create the function
-    result = parallel_processing(n,m,data)
-    
-    # TODO: print out the results, each pair in it's own line
-
-
+    imp = list(map(int, input().split()))
+    n = imp[0]
+    m = imp[1]
+    data = list(map(int, input().split()))
+    result = parallel_processing(n, m, data)
+    for value in result:
+        print(value[0], value[1])
 
 if __name__ == "__main__":
     main()
